6a1a486dba909baf4bbff4bb16090ee1d34d64c3
NelsonVargas15 <var.nelson15@gmail.com>
2024-02-27 01:59:07 -0300

    feat: modify the navbar

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,12 +1,7 @@
+import "../assets/css/contador.css";
+import PropTypes from "prop-types";
 
-import "../assets/css/contador.css"; 
-import PropTypes from 'prop-types';
-
-const Contador = ({ numClics }) => (
-  <div className='contador'>
-    {numClics}
-  </div>
-);
+const Contador = ({ numClics }) => <div className="contador">{numClics}</div>;
 
 Contador.propTypes = {
   numClics: PropTypes.number.isRequired,
--- src/components/CustomerNavbar.jsx
+++ src/components/CustomerNavbar.jsx
@@ -1,28 +1,28 @@
-
 import PropTypes from 'prop-types';
-import { Navbar, Nav , Header} from 'rsuite';
+import { Navbar, Nav, Header } from 'rsuite';
 import HomeIcon from '@rsuite/icons/legacy/Home';
 import CogIcon from '@rsuite/icons/legacy/Cog';
 
 const CustomNavbar = ({ onSelect, activeKey, ...props }) => {
   return (
     <Header className="CustomNavbar" id="navbar">
-    <Navbar {...props}>
-      <Navbar.Brand href="#">Mi Aplicación</Navbar.Brand>
-      <Nav onSelect={onSelect} activeKey={activeKey}>
-        <Nav.Item eventKey="1" icon={<HomeIcon />}>Inicio</Nav.Item>
-        <Nav.Item eventKey="2">Noticias</Nav.Item>
-        <Nav.Item eventKey="3">Productos</Nav.Item>
-        <Nav.Menu title="Acerca de">
-          <Nav.Item eventKey="4">Compañía</Nav.Item>
-          <Nav.Item eventKey="5">Equipo</Nav.Item>
-          <Nav.Item eventKey="6">Contacto</Nav.Item>
-        </Nav.Menu>
-      </Nav>
-      <Nav pullRight>
-        <Nav.Item eventKey="7" icon={<CogIcon />}>Configuración</Nav.Item>
-      </Nav>
-    </Navbar>
+      <Navbar {...props}>
+        <Navbar.Brand href="/">My Application</Navbar.Brand>
+        <Nav onSelect={onSelect} activeKey={activeKey}>
+          <Nav.Item eventKey="1" icon={<HomeIcon />}>
+            Home
+          </Nav.Item>
+          <Nav.Item eventKey="2" href="/aboutme">
+            About Me
+          </Nav.Item>
+          <Nav.Item eventKey="3">Upcoming</Nav.Item>
+        </Nav>
+        <Nav pullRight>
+          <Nav.Item eventKey="4" icon={<CogIcon />}>
+            Achievements
+          </Nav.Item>
+        </Nav>
+      </Navbar>
     </Header>
   );
 };
