2611aadf0ba3700286aa5e589c3151d4d3f2e01a
NelsonVargas15 <var.nelson15@gmail.com>
2024-02-27 01:58:36 -0300

    feat: create new page about feat: create new page about me

---
+++ src/page/AboutMe.jsx
@@ -0,0 +1,12 @@
+import { Stack } from "rsuite";
+
+const AboutMe = () => {
+  return (
+    <Stack justifyContent="center" alignItems="center" direction="column">
+      <h1>sobre mi </h1>
+      <Stack spacing={20}></Stack>
+    </Stack>
+  );
+};
+
+export default AboutMe;
